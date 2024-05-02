b22fc69a61dc79c46d956c8dc9e8ac90195f898b
yahia <ya.akermi@esi-sba.dz>
2024-05-02 14:10:57 +0200

    i add validation to both userDto and PostDto

--- src/main/java/com/yahia/forum/dto/UserDto.java
+++ src/main/java/com/yahia/forum/dto/UserDto.java
@@ -1,17 +1,21 @@
 package com.yahia.forum.dto;
 
 import com.yahia.forum.entity.enums.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UserDto {
 
-    //lazm username ykoun unique
 
+    @Pattern(regexp = "(^[a-zA-Z][a-zA-Z0-9_-]{2,19}$)",message = "the username must begin with characters and withing 3 to 20 character")
     private String username;
 
-    //lazm email ykoun unique
+
+    @Email(message = "you should enter a valid email please")
     private String email;
 
+    @Pattern(regexp ="(^(student|teacher|admin)$)",message = "the user Type should be either student or teacher or admin (case sensitive)")
     private UserType userType;
 }
